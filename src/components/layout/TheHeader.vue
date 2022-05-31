d43c1d8e3bf16355f7cca665e77295675c305179
TanmayDey2022 <tanmay.dey@einzigtech.com>
2022-05-31 15:53:29 +0530

    [FIX] Added padding on header

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -14,7 +14,7 @@
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="collapse navbar-collapse ps-5" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto pb-2">
           <li class="nav-item">
             <router-link to="/" class="nav-link" active-class="active">
